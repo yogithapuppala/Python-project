c0385aa5a35f097d41435d1042a6369da4d24931
yogithapuppala <164842152+yogithapuppala@users.noreply.github.com>
2025-11-19 13:29:08 +0530

    Update config.py

--- flask-todo-app/config.py
+++ flask-todo-app/config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'yogitha')
-    AWS_REGION = 'us-east-1c'
+    AWS_REGION = 'us-east-1'
     DYNAMODB_TABLE = 'todo_tasks'
     SNS_TOPIC_ARN = os.getenv("SNS_TOPIC_ARN")
     EMAIL = "yogithapuppala9@gmail.com"
